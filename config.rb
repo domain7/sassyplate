bd547e564ed01aaaeaba45998611baf1e902dc52
Nathan Shubert-Harbison <nathan@domain7.com>
2013-05-23 16:48:09 -0700

    Changed /sass to /scss in config.rg (sorry, Bridie)

--- config.rb
+++ config.rb
@@ -3,7 +3,7 @@
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "stylesheets/css"
-sass_dir = "stylesheets/sass"
+sass_dir = "stylesheets/scss"
 images_dir = "images"
 javascripts_dir = "js"
 
